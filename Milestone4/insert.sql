b4eab75c58e74965ab8b8c300ee67e6cd5caea1b
AlexH0 <alho6756@colorado.edu>
2018-11-13 11:02:24 -0700

    insert

---
+++ Milestone4/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO Data (LocationID, DeviceID, timestamp, Type, Value, Units);
+
+INSERT INTO Data VALUES (3, 3, TO_TIMESTAMP('2018-11-12 13:43:29', 'YYYY-MM-DD HH24:MI:SS'), 'sound', 30, 'Decibels');
+INSERT INTO Data VALUES (3, 3, TO_TIMESTAMP('2018-11-12 13:43:29', 'YYYY-MM-DD HH24:MI:SS'), 'temperature', 71, 'degrees Fahrenheit');
+
+
+INSERT INTO Data VALUES (2, 4, TO_TIMESTAMP('2018-11-14 14:01:15', 'YYYY-MM-DD HH24:MI:SS'), 'sound', 70, 'Decibels');
+INSERT INTO Data VALUES (2, 4, TO_TIMESTAMP('2018-11-14 14:01:15', 'YYYY-MM-DD HH24:MI:SS'), 'temperature', 77, 'degrees Fahrenheit');
+
+
+INSERT INTO Data VALUES (1, 1, TO_TIMESTAMP('2018-11-12 18:24:39', 'YYYY-MM-DD HH24:MI:SS'), 'sound', 7, 'Decibels');
+INSERT INTO Data VALUES (1, 1, TO_TIMESTAMP('2018-11-12 18:24:39', 'YYYY-MM-DD HH24:MI:SS'), 'temperature', 68, 'degrees Fahrenheit');
+
+
+INSERT INTO Data VALUES (1, 1, TO_TIMESTAMP('2018-11-15 11:37:52', 'YYYY-MM-DD HH24:MI:SS'), 'sound', 30, 'Decibels');
+INSERT INTO Data VALUES (1, 1, TO_TIMESTAMP('2018-11-15 11:37:52', 'YYYY-MM-DD HH24:MI:SS'), 'temperature', 72, 'degrees Fahrenheit');
+
+
+INSERT INTO Data VALUES (4, 6, TO_TIMESTAMP('2018-11-16 12:03:03', 'YYYY-MM-DD HH24:MI:SS'), 'sound', 50, 'Decibels');
+INSERT INTO Data VALUES (4, 6, TO_TIMESTAMP('2018-11-16 12:03:03', 'YYYY-MM-DD HH24:MI:SS'), 'temperature', 74, 'degrees Fahrenheit');
